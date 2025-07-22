147efc939b9c1693c1f6b6452c5ef6bc7fe40e41
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-22 16:13:32 +0200

    Create install-init.sh

---
+++ install-init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+source $path/env
+
+cd $path
+git stash push --include-untracked
+git pull
+chmod +x *.sh
+
+cd /root/galileo
+./bin/geth init --datadir /root/0g-home/geth-home ./genesis.json
+./bin/0gchaind init $MONIKER --home /root/tmp
+
+cp /root/tmp/data/priv_validator_state.json /root/0g-home/0gchaind-home/data/
+cp /root/tmp/config/node_key.json /root/0g-home/0gchaind-home/config/
+cp /root/tmp/config/priv_validator_key.json /root/0g-home/0gchaind-home/config/
