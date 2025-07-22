b2e7adc506892bdc9629a47f7d590f3994850ea4
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-22 16:11:43 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+cd /root
+rm -r galileo
+wget -O galileo.tar.gz https://github.com/0glabs/0gchain-NG/releases/download/v1.2.0/galileo-v1.2.0.tar.gz
+tar -xzvf galileo.tar.gz -C ~
+cd galileo
+cp -r 0g-home /root
+sudo chmod 777 ./bin/geth
+sudo chmod 777 ./bin/0gchaind
+
