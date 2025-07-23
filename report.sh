d82d9acb58ae92195a58ee91bfc4ca04cafb218c
Motko <77466238+Motko222@users.noreply.github.com>
2025-07-23 10:10:11 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -15,6 +15,7 @@ errors_geth=$(journalctl -u 0geth.service --since "1 hour ago" --no-hostname -o 
 node_height=$(echo $(( 16#$(curl -s localhost:8745 -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r .result | sed 's/0x//') )))
 chain_height=$(echo $(( 16#$(curl -s https://evmrpc-testnet.0g.ai -H "Content-Type: application/json" -d '{"jsonrpc":"2.0","method":"eth_blockNumber","params":[],"id":1}' | jq -r .result | sed 's/0x//') )))
 diff=$(( $chain_height - $node_height ))
+url=$(wget -qO- eth0.me):8745
 
 status="ok";message="";
 [ $diff -gt 20 ] && status="warning" && message="syncing (behind $diff)";
