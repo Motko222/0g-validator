c19f07395dce111bbaf740e34f38d93ac86bc5f6
Motko <77466238+Motko222@users.noreply.github.com>
2025-09-18 08:18:57 +0200

    Update install-bin.sh

--- install-bin.sh
+++ install-bin.sh
@@ -1,8 +1,9 @@
+tag=v2.0.4
 cd /root
 rm -r galileo
-wget -O galileo.tar.gz https://github.com/0glabs/0gchain-NG/releases/download/v1.2.0/galileo-v1.2.0.tar.gz
+wget -O galileo.tar.gz https://github.com/0glabs/0gchain-NG/releases/download/$tag/galileo-$tag.tar.gz
 tar -xzvf galileo.tar.gz -C ~
-mv galileo-v1.2.0 galileo
+mv galileo-$tag galileo
 cd galileo
 cp -r 0g-home /root
 sudo chmod 777 ./bin/geth
